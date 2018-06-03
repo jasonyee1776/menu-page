4812fe564fc0fd86a9ed3acbf337fa02a3206cdd
Jason Yee <jasonyee1776@gmail.com>
2018-06-02 22:04:03 -0700

    simple webpage with menu button functionality using jQuery

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+$("button").click(function() {
+    $("#hidden-menu").toggleClass("fade animated bounceInLeft");
+    $("#hidden-menu a").toggleClass("animated flipInX");
+});
+
+// $("button").click(function () {
+//     $("#hidden-menu").toggleClass("animated bounceOutLeft");
+// });
+
+
